372c4ce23383127f5b5da369e2b77c1be21b1222
unknown <ahmaat19@gmail.com>
2021-05-23 09:42:09 +0300

    updated footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,11 +5,19 @@ const Footer = () => {
     <footer>
       <div className='container mt-5 pt-5'>
         <div className='row'>
-          <div className='col text-center py-3'>
-            Copyright &copy;{' '}
-            <a href=' https://geeltech.com' target='blank'>
-              https://geeltech.com
-            </a>
+          <div className='col text-center py-3 d-flex justify-content-between'>
+            <span>
+              Copyright &copy;{' '}
+              <a href=' https://farshaxan.media' target='blank'>
+                Farshaxan Media
+              </a>
+            </span>
+            <span>
+              Powered By{' '}
+              <a href=' https://geeltech.com' target='blank'>
+                Geel Tech
+              </a>
+            </span>
           </div>
         </div>
       </div>
